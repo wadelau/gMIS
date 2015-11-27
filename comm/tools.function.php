5a6cfc45ed84277fc8ccca71e439a0c20950a533
bangco <wadelau@gmail.com>
2015-11-27 08:49:50 +0800

    tools update, delete delay func
    tools.redirect,
    tools.alert,
    delete delay,
    delete aync,

--- comm/tools.function.php
+++ comm/tools.function.php
@@ -1,10 +1,13 @@
 <?php
-#sendMail("784345235@qq.com","这是一个测试2","这是测试2");
-# include($_CONFIG['appdir']."/shiduo/fsmail.php");
+#
+# global functions, by wadelau@ufqi.com
+# update Sat Jul 11 09:20:03 CST 2015
+# update 09:56 Tuesday, November 24, 2015
+#
+
 /* get Smarty template file name
    wadelau, Wed Feb 15 09:18:27 CST 2012
-   15:03 Sunday, August 25, 2013
-   */  
+   */
 function getSmtTpl($file, $act){
     $scriptname = explode("/",$file);
     $scriptname = $scriptname[count($scriptname)-1];
@@ -20,7 +23,7 @@ function getSmtTpl($file, $act){
  * @param array $options for cURL 
  * @return string 
  */ 
-function curlPost($url, array $post = NULL, array $options = array()) { 
+function curlPost($url, array $post = NULL, array $options = array()){ 
     $defaults = array( 
             CURLOPT_POST => 1, 
             CURLOPT_HEADER => 0, 
@@ -49,11 +52,9 @@ function curlPost($url, array $post = NULL, array $options = array()) {
  * @para string $body, message body
  * return array(0=>true|false, 1=>array('error'=>'...'));
  */
-# sendMail('784345235@qq.com','784345235@qq.com','784345235@qq.com');
-function sendMail($to,$subject,$body, $from, $local=1){
-   // print_r('to '.$to.' subject '.$subject.' body '.$body.' from '.$from.' local '.$local);
+function sendMail($to,$subject,$body, $from='', $local=0){
     $rtnarr = array();
-	print __FILE__.': 1<br/>';
+	
 	if($local == 0){
 		$from = $from==''?$_CONFIG['adminmail']:$from;
 
@@ -63,52 +64,39 @@ function sendMail($to,$subject,$body, $from, $local=1){
 		$mailstr .= 'From:'.$from.'\n';
 		$mailstr .= '\n';
 		$mailstr .= $body.'\n';
-		print __FILE__.': 2<br/>';
+
 		$tmpfile = "/tmp/".GConf::get('agentalias').".user.reg.mail.tmp";
-		print __FILE__.': 3<br/>';
 		system('/bin/echo -e "'.$mailstr.'" > '.$tmpfile);  
 		system('/bin/cat '.$tmpfile.' | /usr/sbin/sendmail -t &');  
-		print __FILE__.': 4<br/>';
+
 		$rtnarr[0] = true;
     
-	}else if($local == 1){
-		global $_CONFIG;
-		print __FILE__.': 4.8<br/>';
-        #include($_CONFIG['appdir']."/mod/mailer.class.php");
-		include_once($_CONFIG['appdir']."/mod/mailer.class.php");
-		# remedy ##
-        #$_CONFIG['mail_smtp_server'] = "smtp.sina.com";
-        #$_CONFIG['mail_smtp_username'] = "wadelau2013@sina.com";
-        #$_CONFIG['mail_smtp_password'] = "myminina";
-		
-		#$_CONFIG['mail_smtp_server'] = "smtp.qq.com";
-        #$_CONFIG['mail_smtp_username'] = "344063509@qq.com";
-        #$_CONFIG['mail_smtp_password'] = "msd-344063509";
-		
-		#$_CONFIG['mail_smtp_server'] = "smtp.163.com";
-        #$_CONFIG['mail_smtp_username'] = "djclm_VIP@163.com";
-        #$_CONFIG['mail_smtp_password'] = "xxx";
-		
-		$_CONFIG['mail_smtp_server'] = "smtpcom.263xmail.com";
-        $_CONFIG['mail_smtp_username'] = "webmaster@jpi.cn";
-        $_CONFIG['mail_smtp_password'] = "web2516";
-		
+	}
+	else if($local == 1){
+        global $_CONFIG;
+        include($_CONFIG['appdir']."/mod/mailer.class.php");
+
+        $_CONFIG['mail_smtp_server'] = "smtp.163.com";
+        $_CONFIG['mail_smtp_username'] = "wadelau@163.com";
+        $_CONFIG['mail_smtp_password'] = "myminina.123456";
         $_CONFIG['isauth'] = true;
         $_CONFIG['mail_smtp_fromuser'] = $_CONFIG['mail_smtp_username'];
-		print __FILE__.': 4.9<br/>';
+
         $mail = new Mailer($_CONFIG['mail_smtp_server'],25,$_CONFIG['isauth'],$_CONFIG['mail_smtp_username'],$_CONFIG['mail_smtp_password']);
+
         $mail->debug = true;;
         $from==''?'bangco@'.$_CONFIG['agentname']:$from;
         if($_CONFIG['isauth']){
             $from = $_CONFIG['mail_smtp_fromuser'];
         }
-		print __FILE__.': 5<br/>';
+
         #print __FILE__.": from:$from";
-        $rtnarr[0] = $mail->sendMail($to, $from, $subject, $body, 'HTML');	
-		
-	} 
-	print_r($rtnarr);
-	return $rtnarr;
+        
+        $rtnarr[0] = $mail->sendMail($to, $from, $subject, $body, 'HTML');
+
+    } 
+
+    return $rtnarr;
 }
 
 function startsWith($haystack, $needle)
@@ -123,23 +111,28 @@ function endsWith($haystack, $needle)
     $start  = $length * -1; //negative
     return (substr($haystack, $start) === $needle);
 }
+
 function inList($needle, $haystack){
     $pos = strpos(",".$haystack.",", ",".$needle.",");
     return ($pos === false ? false : true);
 }
 
-function mkUrl($file, $req){
+function inString($needle, $haystack){
+    $pos = stripos($haystack, $needle);
+    return ($pos === false ? false : true);
+}
+
+function mkUrl($file, $_REQU){
     $url = $file."?";
     /*
     $noneeddata = array('act','PHPSESSID','JSESSIONID','userufqi','iweb_shoppingcart','iweb_user_id','iweb_user_pwd','iweb_username','iweb_head_ico','iweb_safecode');
-    foreach($req as $k=>$v){
         if(!in_array($k,$noneeddata)){
             $url .= $k."=".$v."&";
         }
     }
     */
     $needdata = array('id','tbl','db','oid','otbl','oldv','field','linkfield','linkfield2','tit','tblrotate');
-    foreach($req as $k=>$v){
+    foreach($_REQU as $k=>$v){
         if(in_array($k, $needdata) || startsWith($k,'pn') || startsWith($k, "oppn")){
             if($k == 'oldv'){
                 $v = substr($v,0,32); # why? Sun Mar 18 20:40:59 CST 2012
@@ -173,6 +166,15 @@ function shortenStr($str, $len=0){
 
 }
 
+function base62x($s,$dec=0,$numType=''){
+    # e.g. base62x('abcd', 0, '8');
+    # e.g. base62x('abcd', 1, '16');
+    $type = "-enc";
+    if($dec == 1){
+        $type = "-dec";
+    }
+    return $s=exec('/www/webroot/tools/base62x '.$type.($numType==''?'':' -n '.$numType).' \''.$s.'\'');
+}
 
 /**
  *	alert
@@ -182,89 +184,86 @@ function shortenStr($str, $len=0){
  *  @timeout
  */
 function alert($str,$type="back",$topWindow="",$timeout=100){
-	echo "<script>".chr(10);
+	echo "<!DOCTYPE html><html><head><title>Alert!</title></head><body><script type=\"text/javascript\">".chr(10);
 	if(!empty($str)){
-		//echo "window.alert(\"警告:\\n\\n{$str}\\n\\n\");".chr(10);
-		echo "window.alert(\"\\n\\n{$str}\\n\\n\");".chr(10);
+		echo "window.alert(\"警告:\\n\\n{$str}\\n\\n\");".chr(10);
 	}
-	
 	#print "window.alert('type:[".$type."]');\n";
-
 	echo "function _r_r_(){";
 	$winName=(!empty($topWindow))?"top":"self";
 	Switch (StrToLower($type)){
-	case "#":
-		break;
-	case "back":
-		echo $winName.".history.go(-1);".chr(10); # why does it backwards 2 steps?
-		break;
-	case "reload":
-		echo $winName.".window.location.reload();".chr(10);
-		break;
-	case "close":
-		echo "window.opener=null;window.close();".chr(10);
-		break;
-	case "function":
-		echo "var _T=new Function('return {$topWindow}')();_T();".chr(10);
-		break;
-		//Die();
-	default:
-		if($type!=""){
-			//echo "window.{$winName}.location.href='{$type}';";
-			echo "window.{$winName}.location=('{$type}');";
-		}
+		case "#":
+			break;
+		case "back":
+			echo $winName.".history.go(-1);".chr(10);
+			break;
+		case "reload":
+			echo $winName.".window.location.reload();".chr(10);
+			break;
+		case "close":
+			echo "window.opener=null;window.close();".chr(10);
+			break;
+		case "function":
+			echo "var _T=new Function('return {$topWindow}')();_T();".chr(10);
+			break;
+			//Die();
+		default:
+			if($type!=""){
+				//echo "window.{$winName}.location.href='{$type}';";
+				echo "window.{$winName}.location=('{$type}');";
+			}
 	}
-
 	echo "}".chr(10);
-
 	//avoid firefox not excute setTimeout
-	echo "if(setTimeout(\"_r_r_()\",".$timeout.")==2){_r_r_();}";
+	echo "if(window.setTimeout(\"_r_r_()\",".$timeout.")==2){_r_r_();}";
 	if($timeout==100){
 		echo "_r_r_();".chr(10);
-	} else {
-		echo "setTimeout(\"_r_r_()\",".$timeout.");".chr(10);
+	}
+	else{
+		echo "window.setTimeout(\"_r_r_()\",".$timeout.");".chr(10);
 	}
 	echo "</script>".chr(10);
+	print "</script></body></html>";
 	exit();
 }
 
-
-
 /** 
- * URL redirect
+ * URL redirect, remedy by wadelau@ufqi.com 09:52 Tuesday, November 24, 2015
  */
 function redirect($url, $time=0, $msg='') {
-    //multi URL addr support
+    //multi URL addr support ?
     $url = str_replace(array("\n", "\r"), '', $url);
-    if (empty($msg))
-        $msg = "系统将在{$time}秒之后自动跳转到{$url}！";
+	if(!inString('://', $url)){ # relative to absolute path
+		$url = $_SERVER['SERVER_NAME'].$url;
+	}
+	if($time < 10){ $time = $time * 1000; } # in case of milliseconds
+	$hideMsg = "<!DOCTYPE html><html><head>";
+	$hideMsg .= "<meta http-equiv=\"refresh\" content=\"{$time};URL='{$url}'\">";
+	$hideMsg .= "</head><body>";  # remedy Mon Nov 23 22:03:24 CST 2015
+    if (empty($msg)){
+        #$msg = "系统将在{$time}秒之后自动跳转到{$url}！";
+		$hideMsg = $hideMsg." <a href=\"".$url."\">系统将在{$time}秒之后自动跳转</a> <!-- {$url}！--> ...";
+	}
+	else{
+		$hideMsg = $hideMsg . $msg;
+	}
+	$hideMsg .= "<script type='text/javascript'>window.setTimeout(function(){window.location.href='".$url."';}, ".$time.");</script>";
+	$hideMsg .= "</body></html>";
     if (!headers_sent()) {
         // redirect
-        if (0 === $time) {
+        if (1 || 0 === $time) {
             header("Location: " . $url);
-        } else {
-            header("refresh:{$time};url={$url}");
+			print $hideMsg;
+        } 
+        else if(0){  # Refresh in HTTP is non-standard.
+            header("Refresh:{$time};url={$url}");
             echo($msg);
         }
         exit();
-    } else {
-        $str = "<meta http-equiv='Refresh' content='{$time};URL={$url}'>";
-        if ($time != 0)
-            $str .= $msg;
-        exit($str);
-    }
-}
-
-
-function isEmail($email){
-    if(!preg_match('|^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]{2,})+$|i', $email)){
-    
-      return 0;
-    
-    }else{
-      
-      return 1;
-
+    } 
+    else{
+        print $hideMsg;
+        exit();
     }
 }
 
@@ -281,19 +280,24 @@ function isImg($file){
 
 }  
 
+function isEmail($email){
+    if(!preg_match('|^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]{2,})+$|i', $email)){
+    	return 0;
+    }
+    else{
+      return 1;
+    }
+}
+
 # get Ids list from array|hash
 # by wadelau@ufqi.com, Sat Jul 11 09:21:13 CST 2015
 function getIdList($iarray, $ikey){
-	#$tmpIds = "999999,";
-	$tmpIds = "";
+	$tmpIds = "999999,";
 
-	if(count($iarray) > 0){
-		foreach($iarray as $k=>$v){
-			$tmpIds .= $v[$ikey].",";
-		}
+	foreach($iarray as $k=>$v){
+		$tmpIds .= $v[$ikey].",";
 	}
-	#$tmpIds = substr($tmpIds, 0, strlen($tmpIds)-1);
-	$tmpIds .= "999999";
+	$tmpIds = substr($tmpIds, 0, strlen($tmpIds)-1);
 
 	return $tmpIds;
 
@@ -344,6 +348,3 @@ function debug($obj, $tag='', $output=null){
 	}
 	
 }
-
-
-?>
