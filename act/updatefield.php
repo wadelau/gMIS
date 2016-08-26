cb7a4ecbe9aaa4302fc51d96214bc8ae2b3055fc
Wadelau <wadelau@gmail.com>
2016-08-26 18:05:11 +0800

    updatefield imprvs.

--- act/updatefield.php
+++ act/updatefield.php
@@ -4,7 +4,7 @@ $field = $_REQUEST['field'];
 
 # remedy by wadelau Tue Jun 30 16:17:53 CST 2015
 # same as act/doaddmodi.php
-$fieldv = urldecode(trim($_REQUEST[$field]));
+$fieldv = urldecode(trim($_REQUEST[$field]==''?$hmfield[$field."_default"]:$_REQUEST[$field]));
 if(1){
 	if(strpos($fieldv,"<") !== false){ # added by wadelau on Sun Apr 22 22:09:46 CST 2012
 		if($fieldInputType == 'textarea'){	
@@ -17,12 +17,18 @@ if(1){
 			$fieldv = str_replace("<","&lt;", $fieldv);
 		}
 	}
+	else if(strpos($fieldv,"&lt;") !== false){
+		#error_log(__FILE__.": 0 $fieldv");
+		$fieldv = preg_replace("/&lt;[^>]+?>/", "", $fieldv); # remedy on Fri, 26 Aug 2016 16:32:13 +0800
+		$fieldv = str_replace("&nbsp;", "", $fieldv);
+		#error_log(__FILE__.": 1 $fieldv");
+	}
 	if(strpos($fieldv, "\n") !== false){
 		$fieldv = str_replace("\n", "<br/>", $fieldv);
 	}
 }
 
-$gtbl->set($field, $_REQUEST[$field]==''?$hmfield[$field."_default"]:$_REQUEST[$field]);
+$gtbl->set($field, $fieldv);
 $gtbl->setId($id);
 
 $hm = $gtbl->setBy("$field", "");
